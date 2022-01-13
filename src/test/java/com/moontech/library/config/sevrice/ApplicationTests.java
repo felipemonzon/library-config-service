96eee370495d89f8f0ea94170872365777effbf3
Felipe Monzón <felipemonzon2705@gmail.com>
2022-01-12 23:31:15 -0700

    first commit
    first commit

---
+++ src/test/java/com/moontech/library/config/sevrice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moontech.library.config.sevrice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
